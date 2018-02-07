1384c3140af11234b2607c41ab5c96b9217f9988
Peter Van Ausdeln <pvanaus@ucdavis.edu>
2018-02-06 22:15:08 -0800

    I think it works
    Haven't verified multithreadedness, too tired rn

--- HW2/Peter/HwkIIServer.py
+++ HW2/Peter/HwkIIServer.py
@@ -2,40 +2,104 @@ import socket # networking module
 import sys
 import os
 import threading
+
 class srvr(threading.Thread):
+    origHost = None
+    FPList = {}
     threadLock = threading.Lock()
     id = 0
     def __init__(self,newConn):
         threading.Thread.__init__(self)
         self.myid = srvr.id
         srvr.id += 1
+	self.origHost = socket.gethostname()
         self.clientConn = newConn
+    
+    def toHashHost(self,fileName):
+	if self.origHost != socket.gethostname():
+	    os.system("exit &")
+	    os.chdir("/tmp")
+	
+	if str(socket.gethostname) != serHosts[hash(fileName) % len(serHosts)]:
+	    os.system("ssh "+serHosts[hash(fileName) % len(serHosts)]+" &")
+	    os.chdir("/tmp")
+	
+    
+    def getFunc(self, data):
+    	func = data.split("(")[0]
+    	fileName = data.split("(")[1].split(",")[0].split(")")[0]
+    	try:
+            bytesRW = data.split(",")[1].split(")")[0]
+    	except:
+            bytesRW = ""
+    	return (func, fileName, bytesRW)
+    
+    def dOpen(self,fileName):
+	self.toHashHost(fileName)
+	try:
+	    a = open(fileName, 'r+')
+	except IOError:
+	    a = open(fileName, 'a+')
+	self.FPList[fileName] = a
+	return "Opened "+fileName
+    
+    def dRead(self,fileName,bytesToRead):
+	self.toHashHost(fileName)
+	fp = self.FPList[fileName]
+	readBytes = fp.read(int(bytesToRead))
+	return readBytes
+    
+    def dWrite(self, fileName, bytesToWrite):
+	self.toHashHost(fileName)
+	fp = self.FPList[fileName]
+	fp.write(bytesToWrite)
+	return "Wrote \""+bytesToWrite+"\" to "+fileName
+    
+    def dClose(self, fileName):
+	self.toHashHost(fileName)
+	print os.getcwd()
+	fp = self.FPList[fileName]
+	fp.close
+	del self.FPList[fileName]
+	return "Closed "+fileName
+    
     def run(self):
-        username = self.clientConn.recv(1000)
-        while 1:
-            clientInput = self.clientConn.recv(1000)
-            if clientInput == "":
+	os.chdir("/tmp")
+	while 1:
+	    clientInput = self.clientConn.recv(1000) 
+	    if clientInput == "" or clientInput == "done":
                 break
+	    os.getcwd()
+	    clientInput = self.getFunc(clientInput)
+	    if clientInput[0] == "dOpen":
+		clientOutput = self.dOpen(clientInput[1])
+	    elif clientInput[0] == "dRead":
+		clientOutput = self.dRead(clientInput[1],clientInput[2])
+	    elif clientInput[0] == "dWrite":
+		clientOutput = self.dWrite(clientInput[1],clientInput[2])
+	    elif clientInput[0] == "dClose":
+		clientOutput = self.dClose(clientInput[1])
+	    else:
+		clientOutput = "Failed Command"
             srvr.threadLock.acquire()
-            clientInput = os.popen("ssh "+username+"@pc34.cs.ucdavis.edu -t \"cd /tmp; ls\"").read()
-            print clientInput
-            self.clientConn.send(clientInput)
+            self.clientConn.send(clientOutput)
             srvr.threadLock.release()
         self.clientConn.close()
 
 def sysStart(hostList, portNum):
-    global serSock, serPort, threadList
+    global serSock, serPort, serHosts, threadList
     serSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serPort = portNum
+    serHosts = hostList
     serSock.bind(("", serPort))
     serSock.listen(5)
     threadList = []
 
     for i in range(5):
-        clientConn,address = serSock.accept()
-        newClient = srvr(clientConn)
-        threadList.append(newClient)
-        newClient.start()
+	clientConn,address = serSock.accept()
+	newClient = srvr(clientConn)
+    	threadList.append(newClient)
+    	newClient.start()
 
     serSock.close()
 
