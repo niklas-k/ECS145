9a6daa3abe449ac153e804884cad8958339f5881
Peter Van Ausdeln <pvanaus@ucdavis.edu>
2018-02-08 09:52:54 -0800

    comments added, debugging statements removed

--- HW2/Peter/HwkII.py
+++ HW2/Peter/HwkII.py
@@ -2,23 +2,20 @@ import socket
 import sys
 
 global cliHost, cliPort, cliConn
-cliConn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cliHost = sys.argv[1]
-cliPort = int(sys.argv[2])
-cliConn.connect((cliHost, cliPort))
+cliConn = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # set up client socket
+cliHost = sys.argv[1] # host name
+cliPort = int(sys.argv[2]) # port num
+cliConn.connect((cliHost, cliPort)) # connect
 
-i = 0
 while(1):
     input = raw_input(">> ")
-    if input == "done":
+    if input == "done": # end client
         break
-    cliConn.send(input)
-    data = cliConn.recv(1000000)
-    i += 1
+    cliConn.send(input) # send input
+    data = cliConn.recv(1000000) # receive output
     print data
-    if not data:
+    if not data: # kill client if nothing received
         break
-    print 'received', len(data), 'bytes'
 
-cliConn.close()
+cliConn.close() # official close of client
 
