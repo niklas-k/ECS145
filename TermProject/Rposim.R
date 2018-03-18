8fbc17f5933d118562127d886ce7977e2b29af5f
jue-henry <juehenry@yahoo.com>
2018-03-17 22:29:19 -0700

    skeleton

---
+++ TermProject/Rposim.R
@@ -0,0 +1,10 @@
+#used so that each thread shared memory
+library(bigmemory)
+
+#start of simulator
+newsim() <- function(processVec, endOfSim) {
+    #creation of OS, managaes time and waking up each thread
+    
+
+    #creation of a thread for each item in processVec and manages each process
+}
